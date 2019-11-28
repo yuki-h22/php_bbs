d4307c67742146cecd4120b0ad9e035aff1f4158
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 15:04:01 +0900

    Update LoginController.php

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use socialite;
+use Socialite;
 use Illiminate\Http\Request;
 
 
