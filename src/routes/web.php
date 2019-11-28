535e696c4cf78cee3d40db7b1bd8ae72542b55b1
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 14:52:51 +0900

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/user', 'UserController@index');
 Route::get('/bbs', 'BbsController@index');
 Route::post('/bbs','BbsController@create');
+
+Route::get('github', 'Github\GithubController@top');
+Route::post('github/issue', 'Github\GithubController@createIssue');
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
