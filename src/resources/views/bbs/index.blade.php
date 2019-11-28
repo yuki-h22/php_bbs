62b117aa65840475b62248e954e0993fc3734f03
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 14:37:15 +0900

    Update index.blade.php

--- src/resources/views/bbs/index.blade.php
+++ src/resources/views/bbs/index.blade.php
@@ -16,10 +16,12 @@
 @endif
 
 
-@isset($name, $comment)
-<h2>{{ $name }}さんの直前の投稿</h2>
-{{ $comment }}
-<br><hr>
+@isset($bbs)
+@foreach ($bbs as $d)
+    <h2>{{ $d->name }}さんの投稿</h2>
+    {{ $d->comment }}
+    <br><hr>
+@endforeach
 @endisset
 
 <!-- フォームエリア -->
