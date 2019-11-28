33e75eaeaf772771a4283bbff9fbc188f605c068
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 11:57:01 +0900

    Update BbsController.php

--- src/app/Http/Controllers/BbsController.php
+++ src/app/Http/Controllers/BbsController.php
@@ -7,4 +7,19 @@ class BbsController extends Controller
     public function index(){
         return view('bbs.index');
     }
+
+    public function create(Request $request){
+        $request->validate([
+            'name' => 'required|max:10',
+            'comment' => 'required|min:5|max:140',
+        ]);
+        // 投稿内容の受け取って変数に入れる
+        $name = $request->input('name');
+        $comment = $request->input('comment');
+
+        return view('bbs.index')->with([
+            "name" => $name,
+            "comment" => $comment,
+        ]);
+    }
 }
