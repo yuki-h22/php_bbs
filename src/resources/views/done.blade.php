7ca3007264c6b4c2b8e2a8292851bf9377b91cc1
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 15:03:56 +0900

    Create done.blade.php

---
+++ src/resources/views/done.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>github</title>
+    </head>
+    <body>
+        {{ $response }}
+        つくったよ
+    </body>
+</html>
