2a4412373835d7b6c697f6d4b7ba84ad860e9355
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 14:52:53 +0900

    Update LoginController.php

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use socialite;
+use Illiminate\Http\Request;
+
 
 class LoginController extends Controller
 {
@@ -35,5 +38,28 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+        /**
+     * GitHubの認証ページヘユーザーをリダイレクト
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->scopes(['read:user', 'public_repo'])->redirect(); 
+    }
+
+    /**
+     * GitHubからユーザー情報を取得
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback(Request $request)
+    {
+        $user = Socialite::driver('github')->user();
+
+        $request->session()->put('github_token', $user->token);
+        return redirect('github');
     }
 }
